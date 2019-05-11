6c35cc8517b90d41d57385479f124ad1076a5a58
Khussal "Kus" Zamlahani <zamlahani@gmail.com>
2019-05-11 11:25:07 +0700

    reconfig home url

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -80,6 +80,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<code>application/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<?php
+			echo $_SERVER['HTTP_HOST'];
+		?>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
